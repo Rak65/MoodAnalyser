cec1e0372d31acd1a1c43eea239eebe808a6f150
Rak65 <rakeshp6505@gmail.com>
2023-11-20 00:33:35 +0530

    [Added] : Refactor code to take the mood message in constructor.

--- main/java/com/moodanalyzer/MoodAnalyser.java
+++ main/java/com/moodanalyzer/MoodAnalyser.java
@@ -3,6 +3,9 @@ package com.moodanalyzer;
 public class MoodAnalyser {
     private String message;
 
+    public MoodAnalyser() {
+        this.message = "";
+    }
     public MoodAnalyser(String message) {
         this.message = message;
     }
